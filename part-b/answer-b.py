1768dd380a912aa4c6d4df63fcec6a15ad5f0558
s-c-t <sthomas@g.hmc.edu>
2020-01-23 06:41:50 +0000

    completed part b

--- part-b/answer-b.py
+++ part-b/answer-b.py
@@ -4,9 +4,17 @@ A = BoolVar('A')
 B = BoolVar('B')
 C = BoolVar('C')
 
-# write code using A, B, and C, along with 
+# write code using A, B, and C, along with
 # the classes from propositional_logic.py
 # and the .format() mathod to output the
 # following expression:
 
 # (((A => B) & (B => C)) => (A => C))
+ab = Implies(A, B)
+bc = Implies(B, C)
+ac = Implies(A, C)
+
+f1 = And(ab, bc)
+f2 = Implies(f1, ac)
+
+print(f2.format())
